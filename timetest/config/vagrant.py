732225c089fff8ecb807fd51adeda8d93aa0d78f
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2011-07-15 11:49:31 +0300

    initial

---
+++ timetest/config/vagrant.py
@@ -0,0 +1,13 @@
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'timetest',                  
+        'USER': 'timetest',
+        'PASSWORD': 'ChSr3IlaCx',
+    }
+}
